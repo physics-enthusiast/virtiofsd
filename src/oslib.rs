ada5b248c67fc864809e0aa13131bd5c9caa98df
German Maglione <gmaglione@redhat.com>
2022-10-18 20:59:14 +0200

    passthrough: Replace `openat(2)` with `openat2(2)` if available
    Although it is not a sandboxing solution Using `openat2(2)` with
    `RESOLVE_IN_ROOT` and `RESOLVE_NO_MAGICLINKS` add a bit more security
    especially if running as non-root and no sandboxing option is available.
    
    This was requested to be able to run inside an OpenShift unprivileged
    pod where "virtiofsd is already in a container". The OSP seccomp policy
    denies CLONE_NEWUSER and NO_NEW_PRIVILEGES is turn on by default.
    
    Signed-off-by: German Maglione <gmaglione@redhat.com>

--- src/oslib.rs
+++ src/oslib.rs
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
-use std::ffi::CString;
+use std::ffi::{CStr, CString};
 use std::io::{Error, Result};
-use std::os::unix::io::RawFd;
+use std::os::unix::io::{AsRawFd, RawFd};
 
 // A helper function that check the return value of a C function call
 // and wraps it in a `Result` type, returning the `errno` code as `Err`.
@@ -11,6 +11,45 @@ fn check_retval<T: From<i32> + PartialEq>(t: T) -> Result<T> {
         Err(Error::last_os_error())
     } else {
         Ok(t)
+    }
+}
+
+/// Simple object to collect basic facts about the OS,
+/// such as available syscalls.
+pub struct OsFacts {
+    pub has_openat2: bool,
+}
+
+#[allow(clippy::new_without_default)]
+impl OsFacts {
+    /// This object should only be constructed using new.
+    #[must_use]
+    pub fn new() -> Self {
+        // Checking for `openat2()` since it first appeared in Linux 5.6.
+        // SAFETY: all-zero byte-pattern is a valid `libc::open_how`
+        let how: libc::open_how = unsafe { std::mem::zeroed() };
+        let cwd = CString::new(".").unwrap();
+        // SAFETY: `cwd.as_ptr()` points to a valid NUL-terminated string,
+        // and the `how` pointer is a valid pointer to an `open_how` struct.
+        let fd = unsafe {
+            libc::syscall(
+                libc::SYS_openat2,
+                libc::AT_FDCWD,
+                cwd.as_ptr(),
+                std::ptr::addr_of!(how),
+                std::mem::size_of::<libc::open_how>(),
+            )
+        };
+
+        let has_openat2 = fd >= 0;
+        if has_openat2 {
+            // SAFETY: `fd` is an open file descriptor
+            unsafe {
+                libc::close(fd as libc::c_int);
+            }
+        }
+
+        Self { has_openat2 }
     }
 }
 
@@ -97,4 +136,72 @@ impl Drop for ScopedUmask {
     fn drop(&mut self) {
         umask(self.umask);
     }
+}
+
+/// Safe wrapper around `openat(2)`.
+///
+/// # Errors
+///
+/// Will return `Err(errno)` if `openat(2)` fails,
+/// see `openat(2)` for details.
+pub fn openat(dir: &impl AsRawFd, pathname: &CStr, flags: i32, mode: Option<u32>) -> Result<RawFd> {
+    let mode = u64::from(mode.unwrap_or(0));
+
+    // SAFETY: `pathname` points to a valid NUL-terminated string.
+    // However, the caller must ensure that `dir` can provide a valid file descriptor.
+    check_retval(unsafe {
+        libc::openat(
+            dir.as_raw_fd(),
+            pathname.as_ptr(),
+            flags as libc::c_int,
+            mode,
+        )
+    })
+}
+
+/// An utility function that uses `openat2(2)` to restrict the how the provided pathname
+/// is resolved. It uses the following flags:
+/// - `RESOLVE_IN_ROOT`: Treat the directory referred to by dirfd as the root directory while
+/// resolving pathname. This has the effect as though virtiofsd had used chroot(2) to modify its
+/// root directory to dirfd.
+/// - `RESOLVE_NO_MAGICLINKS`: Disallow all magic-link (i.e., proc(2) link-like files) resolution
+/// during path resolution.
+///
+/// Additionally, the flags `O_NOFOLLOW` and `O_CLOEXEC` are added.
+///
+/// # Error
+///
+/// Will return `Err(errno)` if `openat2(2)` fails, see the man page for details.
+///
+/// # Safety
+///
+/// The caller must ensure that dirfd is a valid file descriptor.
+pub fn do_open_relative_to(
+    dir: &impl AsRawFd,
+    pathname: &CStr,
+    flags: i32,
+    mode: Option<u32>,
+) -> Result<RawFd> {
+    // `openat2(2)` returns an error if `how.mode` contains bits other than those in range 07777,
+    // let's ignore the extra bits to be compatible with `openat(2)`.
+    let mode = u64::from(mode.unwrap_or(0)) & 0o7777;
+
+    // SAFETY: all-zero byte-pattern represents a valid `libc::open_how`
+    let mut how: libc::open_how = unsafe { std::mem::zeroed() };
+    how.resolve = libc::RESOLVE_IN_ROOT | libc::RESOLVE_NO_MAGICLINKS;
+    how.flags = flags as u64;
+    how.mode = mode;
+
+    // SAFETY: `pathname` points to a valid NUL-terminated string, and the `how` pointer is a valid
+    // pointer to an `open_how` struct. However, the caller must ensure that `dir` can provide a
+    // valid file descriptor (this can be changed to BorrowedFd).
+    check_retval(unsafe {
+        libc::syscall(
+            libc::SYS_openat2,
+            dir.as_raw_fd(),
+            pathname.as_ptr(),
+            std::ptr::addr_of!(how),
+            std::mem::size_of::<libc::open_how>(),
+        )
+    } as RawFd)
 }
