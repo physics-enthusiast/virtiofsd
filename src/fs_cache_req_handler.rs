1ee998c8c107d0d827fc916cb4acb33d39829634
German Maglione <gmaglione@redhat.com>
2023-12-20 16:28:46 +0100

    Update rust-vmm dependencies
    Updating dependencies to:
    - vhost 0.9.0
    - vhost-user-backend 0.11.0
    - virtio-queue 0.10.0
    - vm-memory 0.13.1
    
    This update renames 'slave' to 'backend' and 'master' to 'frontend'.
    
    Signed-off-by: German Maglione <gmaglione@redhat.com>

--- src/fs_cache_req_handler.rs
+++ src/fs_cache_req_handler.rs
@@ -2,9 +2,9 @@ use crate::fuse;
 use std::io;
 use std::os::unix::io::RawFd;
 use vhost::vhost_user::message::{
-    VhostUserFSSlaveMsg, VhostUserFSSlaveMsgFlags, VHOST_USER_FS_SLAVE_ENTRIES,
+    VhostUserFSBackendMsg, VhostUserFSBackendMsgFlags, VHOST_USER_FS_BACKEND_ENTRIES,
 };
-use vhost::vhost_user::{Slave, VhostUserMasterReqHandler};
+use vhost::vhost_user::{Backend, VhostUserFrontendReqHandler};
 
 /// Trait for virtio-fs cache requests operations.  This is mainly used to hide
 /// vhost-user details from virtio-fs's fuse part.
@@ -23,7 +23,7 @@ pub trait FsCacheReqHandler: Send + Sync + 'static {
     fn unmap(&mut self, requests: Vec<fuse::RemovemappingOne>) -> io::Result<()>;
 }
 
-impl FsCacheReqHandler for Slave {
+impl FsCacheReqHandler for Backend {
     fn map(
         &mut self,
         foffset: u64,
@@ -32,30 +32,30 @@ impl FsCacheReqHandler for Slave {
         flags: u64,
         fd: RawFd,
     ) -> io::Result<()> {
-        let mut msg: VhostUserFSSlaveMsg = Default::default();
+        let mut msg: VhostUserFSBackendMsg = Default::default();
         msg.fd_offset[0] = foffset;
         msg.cache_offset[0] = moffset;
         msg.len[0] = len;
         msg.flags[0] = if (flags & fuse::SetupmappingFlags::WRITE.bits()) != 0 {
-            VhostUserFSSlaveMsgFlags::MAP_W | VhostUserFSSlaveMsgFlags::MAP_R
+            VhostUserFSBackendMsgFlags::MAP_W | VhostUserFSBackendMsgFlags::MAP_R
         } else {
-            VhostUserFSSlaveMsgFlags::MAP_R
+            VhostUserFSBackendMsgFlags::MAP_R
         };
 
-        self.fs_slave_map(&msg, &fd)?;
+        self.fs_backend_map(&msg, &fd)?;
         Ok(())
     }
 
     fn unmap(&mut self, requests: Vec<fuse::RemovemappingOne>) -> io::Result<()> {
-        for chunk in requests.chunks(VHOST_USER_FS_SLAVE_ENTRIES) {
-            let mut msg: VhostUserFSSlaveMsg = Default::default();
+        for chunk in requests.chunks(VHOST_USER_FS_BACKEND_ENTRIES) {
+            let mut msg: VhostUserFSBackendMsg = Default::default();
 
             for (ind, req) in chunk.iter().enumerate() {
                 msg.len[ind] = req.len;
                 msg.cache_offset[ind] = req.moffset;
             }
 
-            self.fs_slave_unmap(&msg)?;
+            self.fs_backend_unmap(&msg)?;
         }
         Ok(())
     }
